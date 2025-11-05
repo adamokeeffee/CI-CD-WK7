1a16dc97c13ebf79f29f8fc10aaf68aaec3f39b6
ADAMOKEEFFE-STUDENT <g00434897@atu.ie>
2025-11-05 14:24:23 +0000

    initial commit

---
+++ src/main/java/ie/atu/week7lab/Week7LabApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week7lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week7LabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week7LabApplication.class, args);
+    }
+
+}
